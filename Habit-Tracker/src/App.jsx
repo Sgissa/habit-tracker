4f10c118d02b522ad8aecc8e1afb9da9a32a1cac
Sgissa <salmanegarba721@gmail.com>
2024-03-18 13:25:20 -0400

    tilteNsidebar

--- Habit-Tracker/src/App.jsx
+++ Habit-Tracker/src/App.jsx
@@ -1,14 +1,24 @@
 import react from "react";
 import "./App.css";
-import Habits from "./components/Habits";
+import Habits from "./components/habits";
+import Sidebar from "./components/sidebar";
+import "semantic-ui-css/semantic.min.css";
+import { Grid, Header } from "semantic-ui-react";
 
 function App() {
   return (
     <>
-      <h1 className="title">Habit Tracker</h1>
-      <div className="container">
-        <Habits />
-      </div>
+      <Grid>
+        <Grid.Column width={3}>
+          <Sidebar />
+        </Grid.Column>
+        <Grid.Column width={8} style={{ marginLeft: "-40px" }}>
+          <Header as="h1" icon="book" content="Habit Tracker" style={{}} />
+          <div className="container">
+            <Habits />
+          </div>
+        </Grid.Column>
+      </Grid>
     </>
   );
 }
